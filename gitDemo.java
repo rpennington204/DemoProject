f76f82e01d11ffe453890c469dc8ae3f2d31ecb5
rpennington204 <reecepennington@icloud.com>
2024-04-10 13:40:55 -0400

    Initial Commit

---
+++ gitDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Reece Pennington
+ */
+public class gitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Dr.Johnson! This is Reece Pennington.");
+
+	}
+
+}
